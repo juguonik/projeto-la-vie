1b50c00a5d4b433760b21f880a8069a052cb8bc0
João Pedro Menezes <100096496+DevJohnny1@users.noreply.github.com>
2022-05-03 19:59:12 -0300

    tirando codigo comentado

--- src/controllers/atendimento.controller.js
+++ src/controllers/atendimento.controller.js
@@ -23,39 +23,3 @@ const AtendimentoController = {
 
 
 module.exports = AtendimentoController;
-
-// //    async cadastrarProduto(req, res) {
-//     const {
-//         nome,
-//         preco,
-//         quantidade,
-//         fabricante_id,
-//         categoria_id,
-//     } = req.body;
-//     const novoProduto = await Produtos.create({
-//         nome,
-//         preco,
-//         quantidade,
-//         fabricante_id,
-//     });
-
-//     const categoria = await Categorias.findByPk(categoria_id);
-
-//     await novoProduto.setCategorias(categoria);
-    
-//     res.json(novoProduto);
-// },
-
-// async deletarProduto(req, res) {
-//     const {
-//         id
-//     } = req.params;
-
-//     await Produtos.destroy({
-//         where: {
-//             id,
-//         },
-//     });
-
-//     res.json('Produto deletado');
-// },
