a36251b5bb35e3041612b2f5dd2a85613ed89bd3
João Menezes <joaosmenezes@hotmail.com>
2022-05-03 19:53:46 -0300

    funcao listar e filtrar por id do atendimento funcionando

--- src/controllers/atendimento.controller.js
+++ src/controllers/atendimento.controller.js
@@ -6,6 +6,56 @@ const AtendimentoController = {
 
        res.json(listaAtendimento);
     },
+
+    async filtrarAtendimentosId(req, res) {
+        try {
+            const listaAtendimento = await Atendimento.findByPk(req.params.id);
+
+            if (listaAtendimento !== null) res.status(200).json(listaAtendimento)
+            else res.status(404).json("Id não encontrado");
+        }
+        catch(error) {
+            return res.status(500).json("erro");
+        }
+    }
 };
 
+
+
 module.exports = AtendimentoController;
+
+// //    async cadastrarProduto(req, res) {
+//     const {
+//         nome,
+//         preco,
+//         quantidade,
+//         fabricante_id,
+//         categoria_id,
+//     } = req.body;
+//     const novoProduto = await Produtos.create({
+//         nome,
+//         preco,
+//         quantidade,
+//         fabricante_id,
+//     });
+
+//     const categoria = await Categorias.findByPk(categoria_id);
+
+//     await novoProduto.setCategorias(categoria);
+    
+//     res.json(novoProduto);
+// },
+
+// async deletarProduto(req, res) {
+//     const {
+//         id
+//     } = req.params;
+
+//     await Produtos.destroy({
+//         where: {
+//             id,
+//         },
+//     });
+
+//     res.json('Produto deletado');
+// },
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const AtendimentoController = require("../controllers/atendimento.controller");
+const AtendimentoController = require("../controllers/atendimento.controller.js");
 
 const routes = express.Router();
 
 routes.get("/atendimentos", AtendimentoController.listar);
+routes.get("/atendimentos/:id", AtendimentoController.filtrarAtendimentosId);
+// routes.post ('/atendimentos', AtendimentoController.atendimentoInfo);
 
 module.exports = routes;
+
